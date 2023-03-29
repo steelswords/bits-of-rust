e32db2fd3a788d4f905fb009f1b47131703c1b74
Tristan Andrus <tristan@tristanandrus.com>
2023-03-29 17:18:40 -0600

    Add a couple of cli arguments to cli_args
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- cli_args/src/main.rs
+++ cli_args/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser, Default, Debug)]
+struct ProgramArgs {
+    // An optional value is denoted by an Option<>
+    name: Option<String>,
+
+    // A flag is a simple bool.
+    #[arg(short, long, action = clap::ArgAction::SetTrue)]
+    verbose: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = ProgramArgs::parse();
+    if let Some(name) = args.name.as_deref() {
+        println!("Hello, {}!", name);
+    }
+    else {
+        println!("Hello, world!");
+    }
+
+    if args.verbose {
+        println!("Someone wanted me to be verbose, but I don't have much to say.¯\\_(ツ)_/¯");
+    }
+    println!("Your arguments are: {:?}", args);
 }
