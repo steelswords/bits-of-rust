4f0b4e3e55e94de7558cd01a66b06105015a5a1a
Tristan Andrus <tristan@tristanandrus.com>
2023-03-30 09:46:12 -0600

    Finish async_simple example
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- async_simple/src/main.rs
+++ async_simple/src/main.rs
@@ -1,32 +1,41 @@
 // This code is largely from the async tutorial @ https://rust-lang.github.io/async-book
 use futures::executor::block_on;
+use rand::Rng;
+use std::thread;
 
 async fn do_something() {
     println!("What do you call a condescending bear? A pan-DUH.");
 }
 
-async unsafe fn think_of_number() -> u8 {
-    static mut number: u8 = 1;
-    number += 1;
-    number
+async fn think_of_number() -> u8 {
+    rand::thread_rng().gen()
 }
 
-async unsafe fn do_a_flip() {
-    for i in 1..think_of_number().await {
-        print!("flip! ");
+async fn do_a_flip(squeak: &str) {
+    for _i in 1..think_of_number().await {
+        print!("{}! ", squeak);
+        thread::sleep_ms(1);
+
     }
     print!("Tada!\n");
 }
 
-async unsafe fn main() {
+async fn async_main() {
     let future = do_something(); // Nothing is printed here. It is all packaged in this future
                                  // call.
     println!("Look, ma! A future to an asynchronous function!");
-    block_on(future); // 'future' is run. You can think of this like a .join() in traditional
+    //block_on(future); // 'future' is run. You can think of this like a .join() in traditional
                       // threading.
 
-    let flip1 = do_a_flip();
-    let flip2 = do_a_flip();
-    futures::join!(flip1, flip2); // Like .await, but activates and waits for multiple
+    let flip1 = do_a_flip("flip");
+    let flip2 = do_a_flip("flop");
+    let flip3 = do_a_flip("flap");
+    let flip4 = do_a_flip("flup");
+    let flip5 = do_a_flip("floup");
+    futures::join!(future, flip1, flip2, flip3, flip4, flip5); // Like .await, but activates and waits for multiple
                                   // futures concurrently
+}
+
+fn main() {
+    block_on(async_main());
 }
